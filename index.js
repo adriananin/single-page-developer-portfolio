67aa26acff044dd253ad987ebb651910cfd77716
adriananin <113365066+adriananin@users.noreply.github.com>
2023-05-15 12:14:43 +0000

    desktop design complete

---
+++ index.js
@@ -0,0 +1,14 @@
+const showOnHover = document.querySelectorAll(".project-img-container");
+const hiddenDiv = document.querySelectorAll(".project-preview");
+
+if (window.innerWidth >= 1440) {
+  showOnHover.forEach((element, index) => {
+    element.addEventListener("mouseenter", () => {
+      hiddenDiv[index].style.display = "flex";
+    });
+
+    element.addEventListener("mouseleave", () => {
+      hiddenDiv[index].style.display = "none";
+    });
+  });
+}
